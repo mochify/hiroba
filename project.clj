b66b6bbfc9882d16775de6334bff18f9e3492b38
William Lee <wlee@mochify.com>
2014-01-25 12:02:24 -0500

    Adding README and basic project structure, along with .travis.yml

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject com.mochify/hiroba "0.1.0-SNAPSHOT"
+  :description "A minimalistic Clojure library for the Foursquare REST API"
+  :url "http://mochify.github.io/hiroba"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [cheshire            "5.3.1"]
+                 [http-kit            "2.1.16"]
+                 [clj-time            "0.6.0"]
+                 [environ             "0.4.0"]]
+  :min-lein-version "2.0.0"
+  :global-vars {*warn-on-reflection* true}
+  :plugins [[codox "0.6.6"]]
+  :codox {:sources ["src"]
+          :output-dir "doc/api"})
